2c68ab94a26d25002aef07c0805a5e9b9ec340ee
FunkyMonkOnKo <115803617+FunkyMonkOnKo@users.noreply.github.com>
2022-11-04 13:28:31 +0100

    Init
    items 2 buckets project for OOP implemantation

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class GameManager : MonoBehaviour
+{
+  public List<GameObject> buckets;
+  public List<GameObject> items;
+  public List<Material> colors;
+  public List<Material> usedColors;
+  private GameObject item1;
+  private GameObject item2;
+  private GameObject item3;
+
+  public GameObject itemBackgroundPlain;
+  public GameObject bucketBackgroundPlain;
+
+  private float xSpawnPoint = 6;
+  private float ySpawnPoint = 2.5f;
+
+  public bool gameOver;
+  public int destroyedItems = 0;
+
+  public ParticleSystem explosionParticle;
+  public Button restartButton;
+
+  // Start is called before the first frame update
+  void Awake()
+  {
+
+
+  }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+  public GameObject SpawnItem(float xValue)
+  {
+    int itemIndex = Random.Range(0, items.Count);
+    GameObject item = Instantiate(items[itemIndex]);
+    items.RemoveAt(itemIndex);
+    item.transform.position = new Vector2(xValue, ySpawnPoint);
+
+    return item;
+  }
+
+  private Material GenarateBackgroundMaterial(GameObject item) {
+    GameObject itemBackground = Instantiate(itemBackgroundPlain);
+    itemBackground.transform.SetParent(item.transform);
+    itemBackground.transform.position = item.transform.position;
+    SpriteRenderer itemBackgroundRenderer = itemBackground.GetComponent<SpriteRenderer>();
+    int colorIndex = Random.Range(0, colors.Count);
+    itemBackgroundRenderer.material = colors[colorIndex];
+
+    var itemScript = item.GetComponent<Item>();
+    itemScript.color = colors[colorIndex].name;
+
+    colors.RemoveAt(colorIndex);
+
+    return itemBackgroundRenderer.material;
+  }
+
+  void GenarateBucketBackgroundMaterial(GameObject bucket)
+  {
+    GameObject bucketBackground = Instantiate(bucketBackgroundPlain);
+    bucketBackground.transform.SetParent(bucket.transform);
+    bucketBackground.transform.position = bucket.transform.position - new Vector3(0, 1.5f, 0);
+    SpriteRenderer itemBucketBackgroundRenderer = bucketBackground.GetComponent<SpriteRenderer>();
+
+    int colorIndex = Random.Range(0, usedColors.Count);
+    itemBucketBackgroundRenderer.material = usedColors[colorIndex];
+    var bucketScript = bucket.GetComponent<Bucket>();
+    bucketScript.color = usedColors[colorIndex].name;
+
+    usedColors.RemoveAt(colorIndex);
+  }
+
+  public void StartGame()
+  {
+    Debug.Log("click");
+    item1 = SpawnItem(-xSpawnPoint);
+    usedColors.Add(GenarateBackgroundMaterial(item1));
+    item2 = SpawnItem(0);
+    usedColors.Add(GenarateBackgroundMaterial(item2));
+    item3 = SpawnItem(xSpawnPoint);
+    usedColors.Add(GenarateBackgroundMaterial(item3));
+
+    foreach (GameObject bucket in buckets)
+    {
+      GenarateBucketBackgroundMaterial(bucket);
+    }
+
+    GameObject.Find("StartScreen").SetActive(false);
+
+  }
+
+  public void GameOver()
+  {
+    gameOver = true;
+    restartButton.gameObject.SetActive(true);
+    Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+  }
+
+  public void RestartGame()
+  {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+  }
+}
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+  private CircleCollider2D itemCollider;
+  private Rigidbody2D itemRB;
+  private Renderer itemRenderer;
+  private GameObject background;
+  public GameManager gameManager;
+  private Renderer itemBackgroundRenderer;
+  private Vector3 defaultPosition;
+  public bool dragged;
+  public bool hoverOnBucket;
+  public string color;
+
+  // Start is called before the first frame update
+  void Start()
+    {
+      gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+      itemCollider = GetComponent<CircleCollider2D>();
+      defaultPosition = transform.position;
+      itemRenderer = GetComponent<Renderer>();
+      background = transform.GetChild(0).gameObject;
+      itemBackgroundRenderer = transform.GetChild(0).gameObject.GetComponent<Renderer>();
+  }
+
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetMouseButtonUp(0))
+    {
+      dragged = false;
+      itemRenderer.sortingLayerName = "default";
+      itemBackgroundRenderer.sortingLayerName = "default";
+    }
+  }
+
+  private void LateUpdate()
+  {
+    if (!dragged && !hoverOnBucket) { transform.position = defaultPosition; }
+  }
+
+  private void OnMouseDrag()
+  {
+    dragged = true;
+    transform.position = GetMousePosition();
+    itemRenderer.sortingLayerName = "dragged";
+    itemBackgroundRenderer.sortingLayerName = "dragged";
+  }
+
+  private Vector3 GetMousePosition()
+  {
+    var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    mousePos.z = 0;
+    return mousePos;
+  }
+
+  private void OnTriggerEnter2D(Collider2D collision)
+  {
+    if (collision.gameObject.CompareTag("item") && dragged)
+    {
+      itemCollider.enabled = false;
+    }
+
+    if (collision.gameObject.CompareTag("bucket") && dragged)
+    {
+      hoverOnBucket = true;
+    }
+  }
+
+  private void OnTriggerStay2D(Collider2D collision)
+  {
+    if (collision.gameObject.CompareTag("bucket") && !dragged)
+    {
+      var bucket = collision.GetComponent<Bucket>();
+      if (bucket.color.Contains(color)) { 
+        Destroy(gameObject);
+        gameManager.destroyedItems++;
+        if (gameManager.destroyedItems == 3) { gameManager.GameOver(); }
+      }
+      else { transform.position = defaultPosition; }    
+    }
+
+  }
+
+  private void OnTriggerExit2D(Collider2D collision)
+  {
+    if (collision.gameObject.CompareTag("item") && dragged)
+    {
+      itemCollider.enabled = true;
+    }
+
+    if (collision.gameObject.CompareTag("bucket") && dragged)
+    {
+      hoverOnBucket = false;
+    }
+  }
+
+  public void SetColor(string generatedColor) {
+    color = generatedColor;
+  }
+
+}
